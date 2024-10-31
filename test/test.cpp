e55d1a5e5015459ebe8ebd36f8876f6334885ef6
cheryl3063 <bkevin3063@conestogac.on.ca>
2024-10-31 18:20:41 -0400

    Every test passed, and the "Invalid" error was fixed.

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,16 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" char* determineWinner(const char player1[], const char player2[]);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace test
+namespace GameTest
 {
-	TEST_CLASS(test)
+	TEST_CLASS(GameTest)
 	{
 	public:
-		
+
 		TEST_METHOD(caseSensitivityTest)
 		{
 			// Test case with lowercase inputs
@@ -21,5 +23,21 @@ namespace test
 			Assert::AreEqual("Player1", determineWinner("Rock", "Scissors"));
 
 		}
+		TEST_METHOD(secondPlayerWinTest)
+		{
+			// Test case with player2 As a winner
+			Assert::AreEqual("Player2", determineWinner("Paper", "Scissors"));
+
+		}
+		TEST_METHOD(inputInvalidFunctionality)
+		{
+			// Test case with Invalid Input
+			Assert::AreEqual("Invalid", determineWinner("rock", "Scissors"));
+
+		}
+
+		
+	
+
 	};
 }
